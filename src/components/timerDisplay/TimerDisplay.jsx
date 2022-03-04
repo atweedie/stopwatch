d648f862790084a7cc50309bc687ef5ca6e9d5d6
Adam Tweedie <adam_tweedie@msn.com>
2022-03-04 11:33:23 +0000

    Stopwatch: Adds TimerDisplay components and updates App component

---
+++ src/components/timerDisplay/TimerDisplay.jsx
@@ -0,0 +1,13 @@
+
+const TimerDisplay = () => {
+    const elapsedTime = 'ToDo!'
+
+    return (
+        <h1>
+            {elapsedTime}
+        </h1>
+    )
+}
+    
+
+export default TimerDisplay;
