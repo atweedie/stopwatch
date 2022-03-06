330a6bd2d065713b5779ce42652700fec4d3d6a8
Adam Tweedie <adam_tweedie@msn.com>
2022-03-06 17:05:42 +0000

    Stopwatch: Adds TimerDisplay component with state and useffect hooks

--- src/components/timerDisplay/TimerDisplay.jsx
+++ src/components/timerDisplay/TimerDisplay.jsx
@@ -1,10 +1,19 @@
+import { useEffect, useState } from "react";
 
 const TimerDisplay = () => {
-    const elapsedTime = 'ToDo!'
+    const [elapsedTime, setElapsedTime] = useState(0);
+
+    const date = new Date(elapsedTime);
+
+    useEffect(() => {
+        setTimeout(() => {
+            setElapsedTime(elapsedTime + 10)
+        }, 10);
+    })
 
     return (
         <h1>
-            {elapsedTime}
+            {date.toISOString().substring(12,22)}
         </h1>
     )
 }
